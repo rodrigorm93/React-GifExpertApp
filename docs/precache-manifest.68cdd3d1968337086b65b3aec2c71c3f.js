29bb737b1d0f4783a3c358ec73e83318809ada92
Rodrigo <ra.ramirez1993@gmail.com>
2020-08-30 17:55:54 -0400

    primer commit

---
+++ docs/precache-manifest.68cdd3d1968337086b65b3aec2c71c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ff351caccce4f997ba456eeca3ea70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f24d1020788af16f44b",
+    "url": "/static/css/main.5108af67.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f24d1020788af16f44b",
+    "url": "/static/js/main.b20f629d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
